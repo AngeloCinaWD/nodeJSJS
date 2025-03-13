1e4681176c93528af06d391314d1952026905df2
unknown <a.cina@procreatech.com>
2025-03-13 10:05:30 +0100

    8. Reading and Writing Files

--- 1-node-farm/README.md
+++ 1-node-farm/README.md
@@ -11,3 +11,11 @@
 [Node FS Module](https://www.w3schools.com/nodejs/nodejs_filesystem.asp)
 
 [Node FS Module 2](https://nodejs.org/docs/latest-v22.x/api/fs.html)
+
+[fs readFileSync()](https://nodejs.org/docs/latest-v22.x/api/fs.html#fsreadfilesyncpath-options)
+
+[utf-8](https://www.ionos.it/digitalguide/siti-web/creare-siti/utf-8-codifica-della-comunicazione-digitale-globale/)
+
+[Buffer class](https://nodejs.org/api/buffer.html)
+
+[fs writeFileSync()](https://nodejs.org/docs/latest-v22.x/api/fs.html#fswritefilesyncfile-data-options)
--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,25 +1,19 @@
-// Tramite il comando node in console è possibile aprire Node REPL (Read Eval Print Loop)
-// Qui è possibile scrivere codice JS direttamente nel terminale
-// Per uscire da REPL: exit, .exit, ctrl + c o ctrl + D
-
-// premendo Tab vengono visualizzate tutte le variabili globali disponibili in Node ed i suoi moduli
-
-// _ l'underscore equivale all'ultimo risultato ottenuto (es. faccio 8 + 8 = 16 e dopo _ + 6 = 22, in questo caso _ è 16)
-
-// digitando il nome di una classe col punto e poi tab ho tutti i metodi disponibili per quella classe (es. String.)
-
-// se creo ad esempio un array e poi array. (prova = [1,2] e poi prova.) ho tutti i metodi disponibili che posso eseguire (ad esempio il .map())
-
-// creo una const
-// const hello = 'hello world';
-// console.log(hello);
-
-// normalmente linko il file .js nell'html
-// in node lo posso runnare col comando node nome fileDaRunnare
-
-// i metodi e le funzionalità di node sono all'interno di moduli
-// per utilizzare un modulo devo utilizzare il metodo require() di node (è la stessa cosa del import and export di js, ma js è considerato un modulo all'interno di node)
-// per esportare un modulo utilzzo module.exports: exports.nomeModulo = function () {}; in questo modo esporto come modulo una funzione che fa qualcosa, dove lo devo utilizzare lo importo col require: const nomeCheVoglio = require('path/nomeModulo');
-// per lavorare con il FileSystem del pc ho bisogno del modulo fs di node, un oggetto contenente metodi e proprietà per leggere e scrivere i file nel pc
-// lo salvo in una const
+// importo il FileSystem module
 const fs = require('fs');
+
+// leggo un file tramite readFileSync() (leggo il suo contenuto), api che lavora in modo sincrono
+// primo argomento path del file che mi interessa
+// il secondo sono options, devo specificare l'encoding del file altrimenti mi verrà restituito il file come Buffer (binary data directly)
+// leggo il file di testo e salvo il contenuto in una const
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+console.log(textIn);
+
+// scrivo in un file
+// \n new line: manda a capo
+const textOut = `This is new text: today is ${Date.now()}.\nAvocado is: ${textIn}`;
+console.log(textOut);
+// per scrivere in un file utilizzo l'API di fs writeFileSync(), indico il path e cosa scrivere. se il file non esiste viene creato
+// devo eseguire il comando e basta, non salvo nulla
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log(fs.readFileSync('./txt/output.txt', 'utf-8'));
