53e9ef5dd082264ed1ce83d9f961718ed388c9bd
unknown <a.cina@procreatech.com>
2025-03-18 13:03:23 +0100

    32. The Node.js Event Loop

--- Section 4.js
+++ Section 4.js
@@ -14,3 +14,10 @@
     Process: programma in esecuzione, node ha una classe Process che ci permette di accedere al processo in esecuzione
     Thread: sequenza di istruzioni, Node lavora in single thread, quindi è facilmente bloccabile se c'è qualche errore. Sequenza di esecuzione del thread: Inizializzazione del programma -> esecuzione top-level code (proprietà e metodi legati all'oggetto globale) -> caricamento dei moduli -> registro delle callback -> inizia l'event-loop. L'event loop non riesce a gestire tutti i tasks e quindi alcuni (quelli più pesanti) vengono dirottati al thread pool, un'architettura fornita da libuv che gestisce 4 Thread separati. Questo avviene per evitare che tasks difficili e pesanti possano bloccare l'esecuzione del programma che runna nel single thread.
 */
+
+// Lecture32. The Node.js Event Loop
+
+/*
+    Tutto il codice all'interno delle funzioni di callback è considerato "Non top level code". Le callback vengono gestite dall'event-loop tramite Event-driven architecture: evento emesso -> raccolto dall'event loop -> callback eseguita
+    Le callback vengono gestite dall'event loop in diverse fasi, secondo una Callback Queue, cioè secondo un ordine di tipo di callback (quelle che riguardano i timer nella prima fase), poi quelle che riguardano l'I/O (seconda fase, quelle che riguardano il sistema, come file e networking). La terza fase è per le setImmediate callbacks (quelle immediatamente dopo le I/O). Nella quarta fase vengono gestite le Close callbacks, quelle che riguardano la chiusura o lo spegnimento del processo.
+*/
