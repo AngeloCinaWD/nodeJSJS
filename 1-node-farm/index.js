55daa3cf5779329289e81f5cf06b8bb44fbe9719
unknown <a.cina@procreatech.com>
2025-03-18 10:57:41 +0100

    23. Recap and What's Next

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -62,21 +62,3 @@ const server = http.createServer((req, res) => {
 server.listen(9200, '0.0.0.0', () => {
   console.log('Listening to requests on port 9200');
 });
-
-// nel package.json le dependencies vengono salvate indicando anche la versione
-// si utilizza la cosiddetta semantic version notation, ad es: ^1.18.11
-// la versione è sempre espressa con 3 numeri
-// il primo numero indica la major version, il secondo la minor version ed il terzo la patch version
-// patch version: versione rilasciata per fixare i bugs (ho la versione 1.18, trovo un bug e lo fixo avrò la 1.18.1, ne trovo un altro e lo fixo 1.18.2 e così via)
-// minor version: vengono rilasciate nuove features nella major, non ci sono breaking changes, cioè le features sono compatibili con le versioni precedenti ad esempio con la 1.17, la 1.16 etc
-// major version viene rilasciata quando alcune nuove features implementate potrebbero non essere compatibili, quindi non funzionare, con le precedenti versioni (ad esempio slugify ora utilizza con la major 1 la funzione slugify(), magari esce la 2 in cui questa funzione ha cambiato nome tipo slugify2() o i parametri che si aspetta sono diversi anche nel nome, ad es. lower diventa lowercase)
-// ^ (accento circonflesso) indica che si accettano minor version e patch differenti della stessa major
-// il comando npm outdated mi dà una tabella coi packages obsoleti
-// per installare una specifica versione di un package runno il comando nom i namePackage@x.x.x
-// se installo slugify tramite npm i slugify avrò l'ultima versione, con la tilde che indica che però accetto anche minor e patch precedenti, ad esempio posso installare npm i slugify@1.0.0
-// se però cambio ^ con la tilde ~ accetto solo patch version
-// esempio installo slugify 1.0.0, la più recente è la 1.6.6, se ho ^1.0.0 e runno npm outdated mi dice che slugify è obsoleto perchè è la versione 1.0.0 e che la voluta sarebbe 1.6.6 (stessa major, accetto tutte le minor e le patch in un eventuale update)
-// se sostituisco ^ con ~ e runno npm outdated mi dice che la voulta (la massima che posso accettare in un eventuale update) è la 1.0.2, accetto solo patch maggiori
-// per aggiornare un package npm update nomePackage, per visualizzare la nuova versione devo runnare npm i
-// l'* prima indica che in caso di update va bene qualsiasi versione, la più aggiornata anche per la major
-// per disinstallare un package npm uninstall namePackage
