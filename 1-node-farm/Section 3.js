4354e2f1e894fd2420009ca123f8923f8e59f891
unknown <a.cina@procreatech.com>
2025-03-18 12:20:33 +0100

    28. Static vs Dynamic vs API

--- 1-node-farm/Section 3.js
+++ 1-node-farm/Section 3.js
@@ -16,15 +16,23 @@
     Il server risponde con una HTTP RESPONSE, molto simile alla request come struttura
     La start line di una response contiene l'http version, lo status code (es: 200, 404 etc) ed uno status message (es. HTTP/1.1 200 OK)
     Il body contiene i dati mandati in risposta (es. l'html da renderizzare, un json etc)
-    
+
     Quando i dati arrivano al client vengono interpretati attraverso HTML, CSS e JS
 */
 
 // LECTURE27. Front-End vs. Back-End Web Development
 /*
-  In generale per Fronte-End ci si riferisce a tutto quello che accade nel web browser, quindi design e building, tutto ciò che è visibile allo user.
-  Per Back-End ci si riferisce a tutto quello che è invisibile allo user. Un web server non è altro che un computer che ha installato un HTTP Server, cioè qualcosa in grado di capire le request che arrivano e gestire le response (è lui che comunica col browser).
-  Nel web server sono salvati i files che servono per costruire una pagina web (immagini, html, css, js etc) e un'app che gestisce questi dat e quelli possibilmente salvati in un database (contiene i dati per riempire i files tipo le stringhe dei titoli, i dati dello user etc)
-
-  NodeJS è un'app che ci aiuta a gestire il nostro web server e comunica con un database tipo MongoDB
+    In generale per Fronte-End ci si riferisce a tutto quello che accade nel web browser, quindi design e building, tutto ciò che è visibile allo user.
+    Per Back-End ci si riferisce a tutto quello che è invisibile allo user. Un web server non è altro che un computer che ha installato un HTTP Server, cioè qualcosa in grado di capire le request che arrivano e gestire le response (è lui che comunica col browser).
+    Nel web server sono salvati i files che servono per costruire una pagina web (immagini, html, css, js etc) e un'app che gestisce questi dat e quelli possibilmente salvati in un database (contiene i dati per riempire i files tipo le stringhe dei titoli, i dati dello user etc) 
+    NodeJS è un'app che ci aiuta a gestire il nostro web server e comunica con un database tipo MongoDB
  */
+
+// LECTURE28. Static vs Dynamic vs API
+/*
+    Static WebSites: il server risponde coi files HTML, CSS e JS per costruire il sito nel browser. Non ci sono app che gestiscono i dati che vengono inviati (invia Static Files secondo una determinata request)
+    
+    Dynamic WebSites: gestiscono il processo SSR (Server Side Rendering), c'è un'app (ad esempio NodeJS) che prende i dati da un db e li usa per costruire i template (HTML, CSS e JS) che vengono restituiti al browser. I dati possono cambiare ad ogni request (ad esempio quando apro una pagina di tweeter trovo nuovi tweet che sono stati salvati nel db). Costruisce dynamic pages.
+
+    API: un API WebSite è sempre dinamico (detto CSR, Client Side Rendered), ma quello che invia al browser sono solo dati, solitamente in formato JSON e non l'intero website. Non vengono inviati file HTML, CSS e JS. Il website viene assemblato nel Client, di solito utilizzando framework come Angular, React o Vue (i templates vengono assemblati nel browser e avviene l'Hydration). I dati restituiti da una API possono essere utilizzati da chiunque la chiami, chiunque ne fa richiesta, non solo un browser, ma anche un'app android, ios, una desktop-app. Questo non avviene con il SSR o con uno static websites perchè solo un browser è in grado di gestire i templates HTML, CSS e JS.
+*/
